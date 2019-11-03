c27c3b885f45c8086a0be6dc02614f15c01bf60b
t2t2 <taavot@gmail.com>
2019-11-03 21:50:49 +0000

    Deploying to gh-pages from master 9f7b473452ea4c5d152220b968998a391b354a3a

---
+++ precache-manifest.f86577d63585c51a1c4d7126c1c3f2fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77906f602e3ee899a535845a24bcc833",
+    "url": "/sl-news/index.html"
+  },
+  {
+    "revision": "09e100aa81bd285eaaa4",
+    "url": "/sl-news/static/css/main.672cfc77.chunk.css"
+  },
+  {
+    "revision": "96645fecad6343dfdcfe",
+    "url": "/sl-news/static/js/2.8b341392.chunk.js"
+  },
+  {
+    "revision": "09e100aa81bd285eaaa4",
+    "url": "/sl-news/static/js/main.04102882.chunk.js"
+  },
+  {
+    "revision": "d4c5e3997df51b08d333",
+    "url": "/sl-news/static/js/runtime-main.1473653f.js"
+  }
+]);
